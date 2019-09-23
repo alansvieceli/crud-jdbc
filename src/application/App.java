27b09844455c3fe09b4447b2fc35b8eb0efab30e
Alan Vieceli <alansvieceli@gmail.com>
2019-09-23 19:57:21 -0300

    recuperar dados

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,40 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DbException;
 
 public class App {
 
 	public static void main(String[] args) {
+		RecuperarDados();
 
+	}
+	
+	private static void RecuperarDados() {
+		
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+	
+			conn = DB.getConnection();
+			try {
+				st = conn.createStatement();
+				rs = st.executeQuery("select * from department");
+				
+				while (rs.next()) {
+					System.out.println(rs.getInt("id") + " - " + rs.getString("name"));
+				}
+				
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		
 	}
 
 }
